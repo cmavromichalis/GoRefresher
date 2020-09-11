1e2b5b0bf11c856509ebb04b7f00004841bb176d
Constantine Mavromichalis <constantine.mavromichalis@cradlepoint.com>
2020-09-10 21:19:40 -0600

    MVP

---
+++ caesarcipher.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type CaesarCipher struct{}
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var isStringAlphabetic = regexp.MustCompile(`^[a-zA-Z ]+$`).MatchString
+
+// validate checks the word passed only has letters `^[a-zA-Z]+$`
+func (cc CaesarCipher) validate(word string) bool {
+	return isStringAlphabetic(word)
+}
+
+func (cc CaesarCipher) calculate(word string, rotateString string, increment bool) (string, error) {
+	if !cc.validate(word) {
+		return "", errors.New("The word given has non-alphabet characters")
+	}
+
+	rotate, err := strconv.Atoi(rotateString)
+	if err != nil {
+		return "", err
+	}
+
+	letters := strings.Split(strings.ToUpper(word), "")
+	var cipherBuilder strings.Builder
+
+	for _, value := range letters {
+		if strings.TrimSpace(value) == "" {
+			cipherBuilder.WriteString(value)
+		} else {
+			position := strings.Index(alphabet, value)
+
+			if increment {
+				cipherBuilder.WriteString(string(alphabet[(position+rotate)%26]))
+			} else {
+				cipherBuilder.WriteString(string(alphabet[(position-rotate)%26]))
+			}
+		}
+	}
+
+	return cipherBuilder.String(), nil
+}
+
+func (cc CaesarCipher) encode(plaintext string, rotate string) (string, error) {
+	return cc.calculate(plaintext, rotate, true)
+}
+
+// decode transforms a CaesarCipher into plaintext by shifting
+// the letters by the specified integer
+func (cc CaesarCipher) decode(plaintext string, rotate string) (string, error) {
+	return cc.calculate(plaintext, rotate, false)
+}
--- main.go
+++ main.go
@@ -1,83 +1,42 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 	"log"
-	"regexp"
-	"strings"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-/*
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-*/
-func main() {
-	/*router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-
-	log.Fatal(http.ListenAndServe(":8080", router))*/
-
+func Decrypt(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	caesar := CaesarCipher{}
+	cipher, err := caesar.decode(ps.ByName("word"), (ps.ByName("rotate")))
 
-	log.Print("ABC")
-	log.Print(caesar.encode("ABC", 1))
-	log.Print("XYZ")
-	log.Print(caesar.encode("XYZ", 1))
-	log.Print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	log.Print(caesar.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 26))
-	log.Print(caesar.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 104))
-	log.Print(caesar.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 27))
-	log.Print(caesar.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 108))
-}
-
-type CaesarCipher struct{}
-
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-var isStringAlphabetic = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
-
-// validate checks the word passed only has letters `^[a-zA-Z]+$`
-func (cc CaesarCipher) validate(word string) bool {
-	return isStringAlphabetic(word)
-}
-
-func (cc CaesarCipher) calculate(word string, rotate int, increment bool) (string, error) {
-	if !cc.validate(word) {
-		return "", errors.New("The word given has non-alphabet characters")
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
 	}
 
-	letters := strings.Split(word, "")
-	var cipherBuilder strings.Builder
+	fmt.Fprint(w, cipher)
+}
 
-	for _, value := range letters {
-		if strings.TrimSpace(value) == "" {
-			cipherBuilder.WriteString(value)
-		} else {
-			position := strings.Index(alphabet, value)
+func Encrypt(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	caesar := CaesarCipher{}
+	cipher, err := caesar.encode(ps.ByName("word"), (ps.ByName("rotate")))
 
-			if increment {
-				cipherBuilder.WriteString(string(alphabet[(position+rotate)%26]))
-			} else {
-				cipherBuilder.WriteString(string(alphabet[(position-rotate)%26]))
-			}
-		}
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
 	}
 
-	return cipherBuilder.String(), nil
+	fmt.Fprint(w, cipher)
 }
 
-func (cc CaesarCipher) encode(plaintext string, rotate int) (string, error) {
-	return cc.calculate(plaintext, rotate, true)
-}
+func main() {
 
-// decode transforms a CaesarCipher into plaintext by shifting
-// the letters by the specified integer
-func (cc CaesarCipher) decode(plaintext string, rotate int) (string, error) {
-	return cc.calculate(plaintext, rotate, false)
+	router := httprouter.New()
+	router.GET("/encrypt/:word/:rotate", Encrypt)
+	router.GET("/decrypt/:word/:rotate", Decrypt)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
